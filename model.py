bb61a8ca71d1c79feffac99e9454ef009e9e2c8d
Pranoy R <pranoyr@Pranoy-2.local>
2021-07-11 23:42:34 +0530

    updated number of classes

--- model.py
+++ model.py
@@ -1,9 +1,12 @@
 import torch
 from torch import nn
 from models.residual_net import ResidualNet
+from models.resnet import ResNet101
 
 
-def generate_model(model_name):
+def generate_model(model_name, num_classes):
 	if model_name == 'ResidualNet-18':
-		model = ResidualNet("ImageNet", 18, 1, "CBAM")
+		model = ResidualNet("ImageNet", 18, num_classes, "CBAM")
+	if model_name == "Resnet-101":
+		model = ResNet101(num_classes)
 	return model
